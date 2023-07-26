10e548b9656f547fcc0a2d4ed871232d91fc3b35
Wallace Queiros <67396393+WallaceQueiros@users.noreply.github.com>
2023-07-26 16:11:34 -0300

    Creation of the looping to carry out the calculations and call the calculate function

--- calculadora.py
+++ calculadora.py
@@ -30,3 +30,19 @@ def calculate():
     num1 = float(input("Enter the first number: "))
     operation = input("Enter the operation (+, -, *, /): ")
     num2 = float(input("Enter the first number:  "))
+
+    if operation == '+':
+        result = sum_numbers(num1, num2)
+    elif operation == '-':
+        result = subtract_numbers(num1, num2)
+    elif operation == '*':
+        result = multiply_numbers(num1, num2)
+    elif operation == '/':
+        result = divide_numbers(num1, num2)
+    else:
+        raise ValueError('Invalid operation')
+
+    print(f"Resultado: {result}")
+
+
+calculate()
