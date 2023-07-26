683fd275c2ee10288e3e9a4c25e859c4625e0472
Wallace Queiros <67396393+WallaceQueiros@users.noreply.github.com>
2023-07-26 15:26:52 -0300

    Create function display_menu

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+def display_menu():
+    print("==== CALCULADORA ====")
+    print("[1]: Soma")
+    print("[2]: Subtração")
+    print("[3]: Multiplicação")
+    print("[4]: Divisão")
+    print("[5]: Sair")
