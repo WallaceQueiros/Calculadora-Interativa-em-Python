188e4cde9329fe8b587062afbe26f4c935d69c1b
Wallace Queiros <67396393+WallaceQueiros@users.noreply.github.com>
2023-07-26 15:42:36 -0300

    Creating a loop to handle the division-by-zero error

--- calculadora.py
+++ calculadora.py
@@ -20,4 +20,7 @@ def multiply_numbers(num1, num2):
 
 
 def divide_numbers(num1, num2):
-    return num1 / num2
+    if num2 != 0:
+        return num1 / num2
+    else:
+        raise ValueError('Cannot divide by zero.')
